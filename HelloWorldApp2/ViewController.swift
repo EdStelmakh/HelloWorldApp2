f34e16063969a9d108b214483bb1e40a05592853
Eduard Stelmakh <morefoto@gmail.com>
2020-08-19 14:53:30 +0300

    Initial Commit

---
+++ HelloWorldApp2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HelloWorldApp2
+//
+//  Created by Macbook Pro on 19.08.2020.
+//  Copyright © 2020 Eduard Stelmakh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
